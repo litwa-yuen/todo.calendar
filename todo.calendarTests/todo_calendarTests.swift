cb7b8f83f950272cbe1b318dcfa3a2bda5830f27
litwa-yuen <litwa.yuen@gmail.com>
2024-11-19 22:47:18 -0500

    init commit

---
+++ todo.calendarTests/todo_calendarTests.swift
@@ -0,0 +1,16 @@
+//
+//  todo_calendarTests.swift
+//  todo.calendarTests
+//
+//  Created by Lit Wa Yuen on 11/19/24.
+//
+
+import Testing
+
+struct todo_calendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
