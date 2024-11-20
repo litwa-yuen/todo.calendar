75dbab77f85c9008a2ed84735038057a254015da
litwa-yuen <litwa.yuen@gmail.com>
2024-11-20 17:22:20 -0500

    Update todo_calendarApp.swift

--- todo.calendar/todo_calendarApp.swift
+++ todo.calendar/todo_calendarApp.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import SwiftData
 import Firebase
 
 
@@ -21,24 +20,10 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct todo_calendarApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
