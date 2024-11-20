354eac1bf5c318cc9bfefd53753d547dac32a410
litwa-yuen <litwa.yuen@gmail.com>
2024-11-19 23:09:03 -0500

    first commit

---
+++ todo.calendar/todo_calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_calendarApp.swift
+//  todo.calendar
+//
+//  Created by Lit Wa Yuen on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct todo_calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
